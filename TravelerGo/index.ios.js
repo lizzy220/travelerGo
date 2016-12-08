20caa69c06a9de610ea234fdcc38782ebb0bd1fa
cgfg <1991chenxi@gmail.com>
2016-12-08 15:46:36 -0600

    remove status bar

--- TravelerGo/index.ios.js
+++ TravelerGo/index.ios.js
@@ -1,51 +1,65 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
+'use strict';
+import React, { Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Navigator,
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
 } from 'react-native';
 
 export default class TravelerGo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.weather_header}>
-          Put weather, location here
-        </Text>
-        <Text style={styles.nearby_posts}>
-          embed nearby postsa here
-        </Text>
-        <Text style={styles.bottom_functions}>
-          put buttons heres
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar
+                backgroundColor="blue"
+                barStyle="light-content"
+                />
+                <Navigator
+                initialRoute={{title:'Travel Go', index: 0}}
+                renderScene={(route, navigator) =>
+                    <View style={styles.container}>
+                        <StatusBar hidden={true} />
+                        <Text style={styles.weather_header}>
+                        Put weather, location here
+                        </Text>
+                        <Text style={styles.nearby_posts}>
+                        embed nearby postsa here
+                        </Text>
+                        <Text style={styles.bottom_functions}>
+                        put buttons heres
+                        </Text>
+                    </View>
+                }
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  weather_header: {
-      flex: 2,
-      backgroundColor: 'powderblue'
-  },
-  nearby_posts: {
-      flex: 10,
-      backgroundColor: 'skyblue',
-  },
-  bottom_functions: {
-      flex: 1,
-      backgroundColor: 'steelblue',
-  }
+    container: {
+        flex: 1,
+    },
+    weather_header: {
+        flex: 3,
+        backgroundColor: 'powderblue'
+    },
+    nearby_posts: {
+        flex: 10,
+        backgroundColor: 'skyblue',
+    },
+    bottom_functions: {
+        flex: 1,
+        backgroundColor: 'steelblue',
+    }
 });
 
 AppRegistry.registerComponent('TravelerGo', () => TravelerGo);
