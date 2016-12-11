4963a0b9e5d5853800764e96340ef0128b006485
lizzy220 <lizzy220@qq.com>
2016-12-10 18:17:23 -0600

    improve home screen

--- TravelerGo/index_components/thumbnail.js
+++ TravelerGo/index_components/thumbnail.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Modal, Text, TouchableHighlight, Slider} from 'react-native';
+import {
+    AppRegistry,
+    StyleSheet,
+    Image,
+    ScrollView,
+    View,
+    TouchableHighlight,
+    Dimensions,
+    Modal,
+    Slider,
+    Text,
+} from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
 import ActionButton from 'react-native-action-button';
 import { Container, Content, Icon, Button} from 'native-base';
 
 
 export default class ThumbNail extends Component {
     constructor(props){
-        super(props);
-        this.onPressCamera=this.props.onPressCamera.bind(this);
+      super(props);
+      this.onPressCamera=this.onPressCamera.bind(this);
+      this.clickPicture=this.clickPicture.bind(this);
+      this.arrangeImages=this.arrangeImages.bind(this);
     }
 
     state = {
@@ -19,8 +34,53 @@ export default class ThumbNail extends Component {
         this.setState({modalVisible: visible});
     }
 
-    render() {
+    onPressCamera(){
+      var self = this;
+      var options = {
+        storageOptions: {
+          skipBackup: true,
+          path: 'images',
+          cameraRoll: true,
+        },
+      };
+      ImagePicker.launchCamera(options, (response) => {
+          if(!response.didCancel){
+            self.props.navigator.push({title: 'cameraPicture', image: response});
+          }
+      });
+    }
 
+    clickPicture(){
+      console.log('clickPicture');
+      this.props.navigator.push({title: 'pictureGo'});
+    }
+
+    arrangeImages(images){
+      var length = images.length;
+      var rows  = Math.floor(length / 2);
+      var ret = [];
+      for(var i=0, j= 0; i < rows; i++){
+        ret.push(
+          <View key={i} style={styles.scrollItemContainer}>
+            {images.slice(j, j+2).map((image) => <TouchableHighlight key={j++} style={styles.imageTouch} onPress={this.clickPicture}><Image style={styles.image} source={require('../place_holder_cat.jpg')}/></TouchableHighlight>)}
+          </View>
+        );
+      }
+      var remain = length % 2;
+      if(remain === 1){
+        ret.push(
+          <View key={rows} style={styles.scrollItemContainer}>
+            <TouchableHighlight key={length-1} style={styles.imageTouch} onPress={this.clickPicture}><Image style={styles.image} source={require('../place_holder_cat.jpg')}/></TouchableHighlight>
+            <View style={styles.image}/>
+          </View>
+        );
+      }
+      return ret;
+    }
+
+    render() {
+        var images=[1,2,3,4,5,6,7,8,9];
+        var scrollItems=this.arrangeImages(images);
         return(
             <View style={styles.thumbNailContainer}>
             <View>
@@ -48,8 +108,9 @@ export default class ThumbNail extends Component {
                     </View>
                 </Modal>
             </View>
-
-            {/*Rest of App come ABOVE the action button component!*/}
+            <ScrollView style={{flex: 1}}>
+              {scrollItems.map((item)=>item)}
+            </ScrollView>
             <ActionButton buttonColor="rgba(231,76,60,1)" >
             <ActionButton.Item buttonColor='#3498db' title="Camera" onPress={this.onPressCamera}>
             <Icon name="md-camera" style={styles.actionButtonIcon} />
@@ -69,50 +130,63 @@ export default class ThumbNail extends Component {
     }
 }
 
+const windowWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
-    thumbNailContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        flex:1,
-        backgroundColor: '#f3f3f3'
-    },
-    modalContainer: {
-        backgroundColor: '#ffffff',
-        marginTop: 250,
-        padding: 20,
-        borderRadius: 4,
-        shadowColor: "#000000",
-        shadowOpacity: 0.5,
-        shadowRadius: 4,
-        shadowOffset: {
-            height: 4,
-            width: 4
-        }
-    },
-    withShadow: {
-        height: 200,
-        width: 200,
-      	backgroundColor: 'grey',
-        borderRadius: 3,
-        shadowColor: "#000000",
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        shadowOffset: {
-            height: 4,
-            width: 4
-        }
-    },
-    titleText: {
-        fontSize: 15,
-        fontWeight: 'bold',
-        paddingBottom: 10
-    },
-    baseText: {
-        fontFamily: 'Cochin',
-    },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+  thumbNailContainer: {
+          flex:1,
+          backgroundColor: '#f3f3f3'
+      },
+      modalContainer: {
+          backgroundColor: '#ffffff',
+          marginTop: 250,
+          padding: 20,
+          borderRadius: 4,
+          shadowColor: "#000000",
+          shadowOpacity: 0.5,
+          shadowRadius: 4,
+          shadowOffset: {
+              height: 4,
+              width: 4
+          }
+      },
+      withShadow: {
+          height: 200,
+          width: 200,
+        	backgroundColor: 'grey',
+          borderRadius: 3,
+          shadowColor: "#000000",
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          shadowOffset: {
+              height: 4,
+              width: 4
+          }
+      },
+      titleText: {
+          fontSize: 15,
+          fontWeight: 'bold',
+          paddingBottom: 10
+      },
+      baseText: {
+          fontFamily: 'Cochin',
+      },
+  scrollItemContainer: {
+    flexDirection: 'row',
+  },
+  imageTouch: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  image: {
+    borderColor: 'white',
+    borderWidth: 1,
+    flex:1,
+    height:windowWidth*0.5,
+  }
 });
